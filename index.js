a4fe218dd6b8c8dc7a1faf4be6d2a665e2f49d67
J.M <kabugijeff@gmail.com>
2023-03-08 11:34:03 +0300

    compeleted assignments

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(word){
+    return word.toUpperCase();
+}
+
+function whisper(strin){
+    return strin.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase())
+    {
+        return "I can't hear you!";
+    }
+    if (string === string.toUpperCase())
+    {
+        return "YES INDEED!";
+    }
+    if (string === "Let's have dinner together!")
+    {
+        return "I would love to!"
+    }
+}
+
+
